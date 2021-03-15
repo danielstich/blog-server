a1b2e3bb7609b7ef5393245701bd35424d689542
danielstich <43919186+danielstich@users.noreply.github.com>
2021-03-15 12:57:51 -0500

    updated index.js to load css and app.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(`${__dirname}/index.html`));
 })
 
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/style.css`));
+})
+
+app.get('/app.js', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/app.js`));
+})
+
 app.listen(port, () =>{
     console.log(`I'm here at port ${port}`);
 })
